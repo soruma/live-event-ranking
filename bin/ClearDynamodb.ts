51e645780a2d1401ddc492854ec4a325b1eccac8
soruma <soruma.net@gmail.com>
2022-03-07 22:53:01 +0900

    Add clear dynamodb script

---
+++ bin/ClearDynamodb.ts
@@ -0,0 +1,4 @@
+await Deno.run({ cmd: ["docker-compose", "stop"] }).status();
+await Deno.run({ cmd: ["rm", "-rf", "tmp/docker/dynamodb"] }).status();
+await Deno.run({ cmd: ["docker-compose", "up", "-d"] }).status();
+await Deno.run({ cmd: ["npx", "sam-dyamodb-create-table", "-e", "http://localhost:8000"] }).status();
