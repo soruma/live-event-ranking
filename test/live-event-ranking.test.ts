987d6dfcb92fa66cf558aaa73a77ca0c631aff1f
soruma <soruma.net@gmail.com>
2022-04-16 02:40:42 +0900

    CDK init

---
+++ test/live-event-ranking.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LiveEventRanking from '../lib/live-event-ranking-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/live-event-ranking-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LiveEventRanking.LiveEventRankingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
