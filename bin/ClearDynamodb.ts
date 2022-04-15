d0f0d120f1407966cc0404a7e940e6e7b4d3c735
soruma <soruma.net@gmail.com>
2022-04-16 02:37:43 +0900

    Clear

--- bin/ClearDynamodb.ts
+++
@@ -1,4 +0,0 @@
-await Deno.run({ cmd: ["docker-compose", "stop"] }).status();
-await Deno.run({ cmd: ["rm", "-rf", "tmp/docker/dynamodb"] }).status();
-await Deno.run({ cmd: ["docker-compose", "up", "-d"] }).status();
-await Deno.run({ cmd: ["npx", "sam-dyamodb-create-table", "-e", "http://localhost:8000"] }).status();
--- lib/deps.ts
+++
@@ -1,8 +0,0 @@
-export type { Context } from "https://deno.land/x/lambda@1.18.0/mod.ts";
-export { format } from "https://deno.land/std@0.91.0/datetime/mod.ts";
-
-export type {
-  DynamoDBClient,
-  ClientConfig,
-} from "https://denopkg.com/chiefbiiko/dynamodb/mod.ts";
-export { createClient } from "https://denopkg.com/chiefbiiko/dynamodb/mod.ts";
